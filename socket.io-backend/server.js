376e050304e82ec271614d509a55002615bb76a3
Maurice Hofmann <maurice.hofmann@fork.de>
2020-02-09 15:20:38 +0100

    first commit

---
+++ socket.io-backend/server.js
@@ -0,0 +1,50 @@
+const io = require("socket.io")();
+const uuidv1 = require("uuid/v1");
+const messageHandler = require("./handlers/message.handler");
+
+const users = {};
+
+function createUserAvatarUrl() {
+    const rand1 = Math.round(Math.random()) * 200 + 100;
+    const rand2 = Math.round(Math.random()) * 200 + 100;
+    return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
+
+function createUsersOnline() {
+    const values = Object.values(users);
+    const onlyWithUsernames = values.filter(u => u.username !== undefined);
+    return onlyWithUsernames;
+}
+
+io.on("connection", socket => {
+    console.log("A user connected!");
+    console.log(socket.id);
+    users[socket.id] = { userId: uuidv1() };
+    socket.on("join", username => {
+        users[socket.id].username = username;
+        users[socket.id].avatar = createUserAvatarUrl();
+        messageHandler.handleMessage(socket, users);
+    });
+    socket.on("disconnect", () => {
+        delete users[socket.id];
+        io.emit("action", { type: "users_online", data: createUsersOnline() })
+    })
+    socket.on("action", action => {
+        switch (action.type) {
+            case "server/hello":
+                console.log("Got Hello Event!", action.data);
+                socket.emit("action", { type: "message", data: "Good day!" });
+                break;
+            case "server/join":
+                console.log("Got join event", action.data);
+                users[socket.id].username = action.data;
+                users[socket.id].avatar = createUserAvatarUrl();
+                io.emit("action", { type: "users_online", data: createUsersOnline() });
+                break;
+            case "server/private-message":
+                console.log("Got a private message", action.data);
+        }
+    })
+});
+
+io.listen(3001);
